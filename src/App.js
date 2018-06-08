410c7671f53e6ab8454b79d50f6bece58a1126ac
Daniel Chavez <bitclaw@gmail.com>
2018-06-08 09:25:15 -0500

    Add enzyme/jest and add .babelrc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
+'use strict';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor (props) {
+        super(props)
+        // this.state = {
+        //     popup: false,
+        //     step: 0
+        // };
+    }
+
+    // const counter = (state=0, action) => {
+    //     switch (action.type) {
+    //         case 'INCREMENT':
+    //             return state +1;
+    //         case 'DECREMENT':
+    //             return state +1;
+    //         default:
+    //             return state;
+    //     }
+    // };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
