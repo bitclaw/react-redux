993179b9d7a5e4d75f43841d98fe6e14a19931e2
Daniel C <bitclaw@gmail.com>
2018-06-09 17:55:28 -0500

    Add static todo list

--- README.old.md
+++ README.old.md
@@ -2,3 +2,4 @@
 React Redux
 
 https://stackoverflow.com/questions/46791263/jest-test-fail-syntaxerror-unexpected-token
+https://hackernoon.com/implementing-basic-component-tests-using-jest-and-enzyme-d1d8788d627a
--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-'use strict';
-
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 
 class App extends Component {
     render() {
@@ -11,6 +9,24 @@ class App extends Component {
                 <header className="App-header">
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <div className="Todo-App">
+                    <form>
+                        <input type="text" />
+                        <div className="Todo-List">
+                            <ul>
+                                <li>
+                                    <input type="checkbox" /> Create a static UI
+                                </li>
+                                <li>
+                                    <input type="checkbox" /> Create a static UI
+                                </li>
+                                <li>
+                                    <input type="checkbox" /> Create a static UI
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                </div>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
