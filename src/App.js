95a7ec556fd97ba18e3b1237d303629b25fa67b7
Daniel C <bitclaw@gmail.com>
2018-06-09 17:47:04 -0500

    Make test pass

--- src/App.js
+++ src/App.js
@@ -5,35 +5,10 @@ import React, { Component } from 'react';
 //import './App.css';
 
 class App extends Component {
-
-    constructor (props) {
-        super(props)
-        // this.state = {
-        //     popup: false,
-        //     step: 0
-        // };
-    }
-
-    // counter = (state=0, action) => {
-    //     switch (action.type) {
-    //         case 'INCREMENT':
-    //             return state +1;
-    //         case 'DECREMENT':
-    //             return state -11;
-    //         default:
-    //             return state;
-    //     }
-    // };
-
-    // expect(
-    //     counter(0,{type: 'INCREMENT'})
-    // ).toEqual(1);
-
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
 
+// describe what we are testing
+describe('App Component', () => {
+    // make our assertion and what we expect to happen
+    it('should render without throwing an error', () => {
+        expect(shallow(<App />).find('div.App').exists()).toBe(true)
+    })
+})
