c84a56453f5b92e5a4149ec44a5c0d5446aa877c
Daniel C <bitclaw@gmail.com>
2018-06-10 08:49:57 -0500

    Create a React Component to Display Status Messages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
+import Message from './components/Message'
 
 class App extends Component {
     render() {
@@ -11,6 +12,7 @@ class App extends Component {
                     <h1 className="App-title">Todo List</h1>
                 </header>
                 <div className="Todo-App">
+                    <Message />
                     <TodoForm />
                     <TodoList />
                 </div>
