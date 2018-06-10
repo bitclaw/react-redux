0675b4c009d6199529e1f0446fe3cfe61250fcb6
Daniel C <bitclaw@gmail.com>
2018-06-10 15:23:55 -0500

    Respond to a Single Redux Action in Multiple Reducers

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,3 +1,6 @@
+import {TODO_ADD,TODOS_LOAD} from './todo'
+
+
 const MESSAGE_SHOW = 'MESSAGE_SHOW'
 
 export const showMessage = (message) => ({type: MESSAGE_SHOW,payload:message})
@@ -6,6 +9,9 @@ export default (state = '',action) => {
     switch (action.type) {
         case MESSAGE_SHOW:
             return action.payload
+        case TODO_ADD:
+        case TODOS_LOAD:
+            return ''
         default:
             return state
     }
