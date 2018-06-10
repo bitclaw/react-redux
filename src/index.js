3cc868fa396cf5088b71b8beeaa3baad02799790
Daniel C <bitclaw@gmail.com>
2018-06-10 01:45:01 -0500

    Use shorthand syntax where the key and the value is the same

--- src/index.js
+++ src/index.js
@@ -7,17 +7,14 @@ import store from './store'
 import {updateCurrent} from './reducers/todo'
 import {bindActionCreators} from 'redux'
 
-// const todoChangeHandler = (value) => store.dispatch(updateCurrent(value))
 
-const actions = bindActionCreators({
-    todoChangeHandler: updateCurrent
-},store.dispatch)
+const actions = bindActionCreators({updateCurrent},store.dispatch)
 
 const render = () => {
     const state = store.getState()
     ReactDOM.render(<App todos = {state.todos}
                          currentTodo={state.currentTodo}
-                         changeCurrent={actions.todoChangeHandler}
+                         changeCurrent={actions.updateCurrent}
     />, document.getElementById('root'));
 }
 
