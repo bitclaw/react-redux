fb49fc584bb9b3a0256fb79faeaab347b4529f7a
Daniel C <bitclaw@gmail.com>
2018-06-10 03:06:34 -0500

    Dispatch Asynchronous Actions with Redux Thunk Middleware

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,16 +1,22 @@
+import {getTodos} from '../lib/todoServices'
+
 const initState = {
-    todos: [
-        {id:1,name:'Create a store', isComplete: true},
-        {id:2,name:'Load state through a store', isComplete: true},
-        {id:3,name:'Handle state changes with Redux', isComplete: false},
-    ],
+    todos: [],
     currentTodo: ''
 }
 
-const CURRENT_UPDATE = 'CURRENT_UPDATE'
 const TODO_ADD = 'TODO_ADD'
+const TODOS_LOAD = 'TODOS_LOAD'
+const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
 export const updateCurrent = (value) => ({type: CURRENT_UPDATE,payload: value})
+export const loadTodos = (todos)  => ({type: TODOS_LOAD, payload: todos})
+export const fetchTodos = () => {
+    return (dispatch) => {
+        getTodos()
+        .then(todos => dispatch(loadTodos(todos)))
+    }
+}
 
 
 export default (state = initState,action) => {
@@ -19,6 +25,8 @@ export default (state = initState,action) => {
             return {...state,todos: state.todos.concat(action.payload)}
         case CURRENT_UPDATE:
             return {...state,currentTodo: action.payload}
+        case TODOS_LOAD:
+            return {...state,todos: action.payload}
         default:
             return state
     }
