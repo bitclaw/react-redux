3416641c67e3a7bccea70235a352dc5a585b3287
Daniel C <bitclaw@gmail.com>
2018-06-10 01:20:49 -0500

    Render React UI Based on State Changes with Subscribe in Redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 
-const state = store.getState()
+const render = () => {
+    const state = store.getState()
+    ReactDOM.render(<App todos = {state.todos} />, document.getElementById('root'));
+}
 
-ReactDOM.render(<App todos = {state.todos} />, document.getElementById('root'));
+render()
+
+store.subscribe(render)
+
+setTimeout(() => {
+    store.dispatch({type: 'TODO_ADD',payload: {id: 4,name:'New Todo',isComplete: false}})
+},1000)
+
 registerServiceWorker();
